ebcf1a8ed09379ef6f0f775b45b64d2359bbafdd
Ibuki Aizawa <ibuki.aizawa@sota.co.jp>
2022-05-07 18:55:54 +0900

    Changed to respond to local IP address

--- app/app.go
+++ app/app.go
@@ -2,11 +2,22 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"net/http"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, World\n")
+
+	netInterfaceAddresses, _ := net.InterfaceAddrs()
+	for _, netInterfaceAddress := range netInterfaceAddresses {
+		networkIp, ok := netInterfaceAddress.(*net.IPNet)
+		if ok && !networkIp.IP.IsLoopback() && networkIp.IP.To4() != nil {
+			ip := networkIp.IP.String()
+			fmt.Fprintf(w, "Hello\nMy Local IP Address is %v\n", ip)
+		}
+	}
+
+	//fmt.Fprintf(w, "Hello, World\n")
 }
 
 func main() {
