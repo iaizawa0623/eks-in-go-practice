5776211e74ee6ee8f49acc45a7159e77620b4945
Ibuki Aizawa <ibuki.aizawa@sota.co.jp>
2022-05-09 03:45:55 +0900

    add comments

--- app/app.go
+++ app/app.go
@@ -6,20 +6,19 @@ import (
 	"net/http"
 )
 
+//	response local ip address
 func handler(w http.ResponseWriter, r *http.Request) {
-
 	netInterfaceAddresses, _ := net.InterfaceAddrs()
 	for _, netInterfaceAddress := range netInterfaceAddresses {
 		networkIp, ok := netInterfaceAddress.(*net.IPNet)
 		if ok && !networkIp.IP.IsLoopback() && networkIp.IP.To4() != nil {
 			ip := networkIp.IP.String()
-			fmt.Fprintf(w, "Hello\nMy Local IP Address is %v\n", ip)
+			fmt.Fprintf(w, "My Local IP Address is %v\n", ip)
 		}
 	}
-
-	//fmt.Fprintf(w, "Hello, World\n")
 }
 
+//	run http server
 func main() {
 	http.HandleFunc("/", handler)
 	http.ListenAndServe(":80", nil)
